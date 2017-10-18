adbf7927b20b955a90aa776fd3babf671b6a8db0
mubaji <951038378@qq.com>
2017-10-18 17:08:43 +0800

    add ioc creator

--- webapi/abuseipdb.py
+++ webapi/abuseipdb.py
@@ -1,15 +1,29 @@
 #coding=utf-8
 import requests
 
+'''
+categories : https://www.abuseipdb.com/categories
+'''
+category = {3: 'Fraud Orders',
+            4: 'DDoS Attack',
+            9: 'Open Proxy',
+            10: 'Web Spam',
+            11: 'Email Spam',
+            14: 'Port Scan',
+            18: 'Brute-Force',
+            19: 'Bad Web Bot',
+            20: 'Exploited Host',
+            21: 'Web App Attack',
+            22: 'SSH',
+            23: 'IoT Targeted'}
+
 url = 'https://www.abuseipdb.com/check/'
-url2 = 'https://www.abuseipdb.com/report/json?'
 API_KEY = '2HwugmLnnW3uv8KEcvLPoL8u6kDG1PGF8fy4VhUp'
 
-def ipcheck(ip, days):
-    gurl = url + '%s/json?key=%s&days=%s'%(ip, API_KEY, days)
+def ipcheck(ip):
+    gurl = url + '%s/json?key=%s'%(ip, API_KEY)
     res = requests.get(gurl)
     print res.content
-def ipreport(ip, categories, comment):
-    gurl = url2 + 'key=%s&category=%s&comment=%s&ip=%s'%(API_KEY, categories, comment, ip)
-    res = requests.get(gurl)
-    print res.content
+
+ipcheck('88.211.129.250')
+
